754d6af9501ad0d7d20e982e5a74a46373001475
Miguel(MgARL) <mgarlcr@gmail.com>
2022-05-05 20:46:21 -0400

    refactored My skills

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -50,7 +50,7 @@ function Contact() {
     <Container style={{ height: '100vh' }} >
       <Row>
         <Col xs={12}>
-          <h1 className='mb-5'>Contact Information</h1>
+          <h1 className='my-2'>Contact Information</h1>
         </Col>
       </Row>
       <Row className='mx-2 py-2'>
@@ -59,15 +59,15 @@ function Contact() {
           <Form>
             <Form.Group controlId='basicForm' className='mb-3'>
               <Form.Label>Your Name:</Form.Label>
-              <Form.Control type='input' placeholder='Enter Your Name' />
+              <Form.Control type='input' placeholder='Enter Your Name'  className='mb-2'/>
               <Form.Label>Your Email:</Form.Label>
-              <Form.Control type='email' placeholder='Enter Your Email' />
+              <Form.Control type='email' placeholder='Enter Your Email' className='mb-3'/>
               <Form.Label>Subject:</Form.Label>
-              <Form.Control type='input' placeholder='Enter Subject' />
+              <Form.Control type='input' placeholder='Enter Subject' className='mb-3'/>
               <Form.Label>Your Message:</Form.Label>
               <Form.Control as='textarea' rows={3} placeholder='Enter Your Message'/>
             </Form.Group>
-            <Button type="submit">Submit form</Button>
+            <Button type="submit">Submit</Button>
           </Form>
         </Col>
         <Col xs={12} lg={6} className='mb-3 py-3'>
--- src/components/MySkills.js
+++ src/components/MySkills.js
@@ -4,6 +4,7 @@ import Col from 'react-bootstrap/Col'
 import { Container } from 'react-bootstrap'
 
 function MySkills() {
+    const skillsArray = ['HTML5', 'CSS3', 'JavaScript', 'ReactJS', 'NodeJS','ExpressJS', 'MongoDB', 'MongooseODM', 'PostgreSQL', 'SequelizeODM', 'git | Github', 'Bootstrap', 'Problem Solver', 'Goal Oriented', 'Detail Oriented']
     return (
         <>
             <Row>
@@ -13,51 +14,13 @@ function MySkills() {
             </Row>
             <Container className='bg-white py-1 rounded'>
                 <Row xs={3} className='mt-3'>
-                    <Col>
-                        <h5>HTML5</h5>
-                    </Col>
-                    <Col>
-                        <h5>CSS3</h5>
-                    </Col>
-                    <Col>
-                        <h5>Javascript</h5>
-                    </Col>
-                    <Col>
-                        <h5>ReactJS</h5>
-                    </Col>
-                    <Col>
-                        <h5>NodeJS</h5>
-                    </Col>
-                    <Col>
-                        <h5>ExpressJS</h5>
-                    </Col>
-                    <Col>
-                        <h5>MongoDB</h5>
-                    </Col>
-                    <Col>
-                        <h5>Mongoose ODM</h5>
-                    </Col>
-                    <Col>
-                        <h5>PostgreSQL</h5>
-                    </Col>
-                    <Col>
-                        <h5>Sequelize ODM</h5>
-                    </Col>
-                    <Col>
-                        <h5>git | Github</h5>
-                    </Col>
-                    <Col>
-                        <h5>Bootstraps</h5>
-                    </Col>
-                    <Col>
-                        <h5>Problem Solver</h5>
-                    </Col>
-                    <Col>
-                        <h5>Goal Oriented</h5>
-                    </Col>
-                    <Col>
-                        <h5>Detail Oriented</h5>
-                    </Col>
+                    {skillsArray.map((skill, i) =>{
+                        return(
+                            <Col key={i}>
+                                <h5>{skill}</h5>
+                            </Col>
+                        )
+                    })}                  
                 </Row>
 
             </Container>
