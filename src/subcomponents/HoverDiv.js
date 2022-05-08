ed091052efc84e4f134eff765e1909348f1fdd37
Miguel(MgARL) <mgarlcr@gmail.com>
2022-05-07 22:56:27 -0400

    Added links on hover each project image

--- src/subcomponents/HoverDiv.js
+++ src/subcomponents/HoverDiv.js
@@ -8,9 +8,9 @@ function HoverDiv({ liveSite, frontEndRepo, backEndRepo, hovering, setHovering, 
     currentElement = Number(currentElement)
     return (
         <div className={!hovering ? 'd-none' : currentElement === index ? 'on-top d-flex align-items-center justify-content-evenly' : 'd-none'  }  onMouseLeave={() => setHovering(false)}>
-            <a href={liveSite} target='_blank' rel="noreferrer"><FaLink style={{ fontSize: '1.5em' }} /></a>
-            <a href={frontEndRepo} target='_blank' rel="noreferrer" ><FaGithub style={{ fontSize: '1.5em' }} /></a>
-            {backEndRepo ? <a href={backEndRepo} target='_blank' rel="noreferrer"><FaGithubSquare style={{ fontSize: '1.7em' }} /></a> : null}
+            <a href={liveSite} target='_blank' rel="noreferrer" style={{textDecoration: 'none'}} ><FaLink style={{ fontSize: '2.1em' }} /></a>
+            <a href={frontEndRepo} target='_blank' rel="noreferrer" style={{textDecoration: 'none'}} ><FaGithub style={{ fontSize: '1.5em' }} /><br/>Front End</a>
+            {backEndRepo ? <a href={backEndRepo} target='_blank' rel="noreferrer" style={{textDecoration: 'none'}}><FaGithubSquare style={{ fontSize: '1.7em' }} /><br/>Back End</a> : null}
         </div>
     )
 }
