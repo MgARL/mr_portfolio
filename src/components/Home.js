a765d46a77ce5a487eb24c28536fb0258cb81c62
Miguel-Ubuntu-lap <mgarlcr@gmail.com>
2022-05-20 12:58:32 -0400

    Added Call to action to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
 
 // My Comps
 import SocialMedia from '../subcomponents/SocialMedia'
 
 function Home() {
   return (
-    <Container style={{height: '100vh'}} className='d-flex flex-column align-items-center justify-content-center'>
+    <Container style={{ height: '100vh' }} className='d-flex flex-column justify-content-center align-itemns-center' >
       <Row>
         <Col xs={12} className="text-center">
           <h1>Hello, I am Miguel Rodriguez</h1>
@@ -16,7 +18,15 @@ function Home() {
       </Row>
       <Row>
         <Col xs={12} className="text-center">
-          <p>I am a passionate web developer that uses my skills to solve problems to bring value to clients and prospects. I am always learning about new and upcoming technologies that can help me bring  a better experience to the end user.</p>
+          <p>I am a passionate web developer with experience in the PERN stack (PostgreSQL, Express, React and Node). I use my skills to solve problems to bring value to clients and prospects. I am always learning about new and upcoming technologies that can help me bring  a better experience to the end user.</p>
+        </Col>
+      </Row>
+      <Row className='d-flex justify-content-center my-3'>
+        <Col xs={12} lg={2} className='d-flex justify-content-center mb-2'>
+          <Button variant="primary" as={Link} to="/contact">Contact Me</Button>
+        </Col>
+        <Col xs={12} lg={2} className='d-flex justify-content-center mb-2' >
+          <Button variant="primary" as={Link} to="/portfolio" >My Projects</Button>
         </Col>
       </Row>
       <SocialMedia />
--- src/subcomponents/SocialMedia.js
+++ src/subcomponents/SocialMedia.js
@@ -6,14 +6,14 @@ import LineIcon from 'react-lineicons'
 function SocialMedia() {
     return (
         <>
-            <Row >
-                <Col xs={4}>
+            <Row  className='d-flex justify-content-center'>
+                <Col xs={4} sm={2} className='d-flex justify-content-center'>
                     <LineIcon name="linkedin-original" style={{ fontSize: "25px", textDecoration: 'none' }} tag="a" href="http://www.linkedin.com/in/miguel-rod-developer/" rel="noreferrer" target="_blank" />
                 </Col>
-                <Col xs={4}>
+                <Col xs={4} sm={2}  className='d-flex justify-content-center'>
                     <LineIcon name="github-original" style={{ fontSize: "25px", textDecoration: 'none' }} tag="a" href="https://github.com/MgARL" rel="noreferrer" target="_blank" />
                 </Col>
-                <Col xs={4}>
+                <Col xs={4}  sm={2} className='d-flex justify-content-center'>
                     <LineIcon name="twitter-original" style={{ fontSize: "25px", textDecoration: 'none' }} tag="a" href="https://twitter.com/mgarl22" rel="noreferrer" target="_blank" />
                 </Col>
             </Row>
