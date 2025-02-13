d9f54a55d9d8dd15ead91f9a945ddab381edc175
Miguel <mro2508@wgu.edu>
2025-02-13 18:03:00 -0500

    Added most recent job expereience and education.

--- src/subcomponents/Education.js
+++ src/subcomponents/Education.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Card from 'react-bootstrap/Card'
-import NCSLogo from '../images/NC_State_logo.jpg'
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import NCSLogo from '../images/NC_State_logo.jpg';
+import pierpontLogo from '../images/logos/PierPontLogo.png';
+import WGULogo from '../images/logos/western-governors-university-logo.png';
 
 function Education() {
 
@@ -14,6 +16,49 @@ function Education() {
                 </Col>
             </Row>
             <Row className='mb-3'>
+                {/* WGU*/}
+                <Col xs={12} sm={4}>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title> 
+                                <Row>
+                                    <Col xs={12} xl={3}>
+                                        <img src={WGULogo} alt="North Carolina State logo" height='80px' className='me-2' />
+                                    </Col>
+                                    <Col xs={12} xl={9} className='d-flex align-items-center'>
+                                        Western Governors University
+                                    </Col>
+                                </Row>
+                            </Card.Title>
+                            <Card.Subtitle className="my-2 text-muted">Graduation: February 2025</Card.Subtitle>
+                            <Card.Text>
+                                Bachelor's of Science: Software Engineering
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                {/* Pierpont  */}
+                <Col xs={12} sm={4}>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title> 
+                                <Row>
+                                    <Col xs={12} xl={3}>
+                                        <img src={pierpontLogo} alt="North Carolina State logo" height='80px' className='me-2' />
+                                    </Col>
+                                    <Col xs={12} xl={8} className='d-flex align-items-center'>
+                                    pierpont community and technical college
+                                    </Col>
+                                </Row>
+                            </Card.Title>
+                            <Card.Subtitle className="my-2 text-muted">Graduation: December 2024</Card.Subtitle>
+                            <Card.Text>
+                                Associates of Applied Science: Board of Governors - Emphasis on Information Systems.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                {/* NC STATE */}
                 <Col xs={12} sm={4}>
                     <Card>
                         <Card.Body>
@@ -27,7 +72,7 @@ function Education() {
                                     </Col>
                                 </Row>
                             </Card.Title>
-                            <Card.Subtitle className="my-2 text-muted">Sep 2021 - Jul 2022</Card.Subtitle>
+                            <Card.Subtitle className="my-2 text-muted">Graduation: July 2022</Card.Subtitle>
                             <Card.Text>
                                 Web Development Bootcamp
                             </Card.Text>
--- src/subcomponents/ProfessionalExp.js
+++ src/subcomponents/ProfessionalExp.js
@@ -3,6 +3,7 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 import ASCLogo from '../images/ASC_Logo_Dark.png'
+import OuiLogo from '../images/logos/Odysseys Logo.png'
 
 function ProfessionalExp() {
   return (
@@ -13,13 +14,33 @@ function ProfessionalExp() {
         </Col>
     </Row>
     <Row className='mb-3'>
+         {/* Odysseys */}
+         <Col xs={12} sm={4}>
+            <Card>
+                <Card.Body>
+                    <Card.Title> <img src={OuiLogo} alt="Odysseys logo" height='80px' className='me-2' /> Odysseys Unlimited </Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Feb 2024 - Currently</Card.Subtitle>
+                    <Card.Text>
+                        Responsibilities include:
+                        <ul>
+                            <li>Assist in the development and enhancement of the internal Customer Relationship Management (CRM) system using Microsoft Stack technologies.</li>
+                            <li>Perform routine maintenance of existing codebases to improve functionality and resolve bugs.</li>
+                            <li>Manage and maintain the SQL Server database associated with the CRM system, including writing
+                            queries and stored procedures.</li>
+                            <li>Implement feedback from users to enhance the functionality and user experience of the CRM system and more.</li>
+                        </ul>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Col>
+        {/* Archetype */}
         <Col xs={12} sm={4}>
             <Card>
                 <Card.Body>
                     <Card.Title> <img src={ASCLogo} alt="ASC logo" height='80px' className='me-2' /> Archetype SC </Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">Sep 2022 - Currently</Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted">Sep 2022 - Feb 2024</Card.Subtitle>
                     <Card.Text>
-                        Designing and sustaining solutions for a diverse clientele by harnessing our expertise within the Microsoft ecosystem, encompassing technologies such as C#, Microsoft SQL Server, Azure DevOps, Azure Cloud Services, and the ASP.NET framework, among others. 
+                        Responsibilities include designing and sustaining solutions for a diverse clientele by harnessing our expertise within the Microsoft ecosystem, encompassing technologies such as C#, Microsoft SQL Server, Azure DevOps, Azure Cloud Services, and the ASP.NET framework, among others. 
                         Utilizing advanced tools including Visual Studio, Visual Studio Code, and SQL Server Management Studio (SSMS), and more.
                     </Card.Text>
                 </Card.Body>
